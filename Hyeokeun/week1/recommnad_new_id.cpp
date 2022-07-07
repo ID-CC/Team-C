1d07a0f3a6d4a1cadc9ee98ce8114466077a5352
Yagoonin <108348352+Yagoonin@users.noreply.github.com>
2022-07-07 21:23:41 +0900

    answer의 length 관리 변수 삭제
    불필요한 계산 삭제

--- Hyeokeun/week1/recommnad_new_id.cpp
+++ Hyeokeun/week1/recommnad_new_id.cpp
@@ -8,14 +8,12 @@ string solution(string new_id) {
     string answer = "";
     
     answer = new_id;
-	int len_id = answer.length();
-
+	
 	if (answer == "" || answer.empty() || answer.length() == 0) {
 		answer = "a";
-		len_id = 1;
 	}
 	
-	for (int i = len_id - 1; i >= 0; i--) {
+	for (int i = answer.length() - 1; i >= 0; i--) {
 		char this_char = answer.at(i);
 		// step 1
 		if (this_char >= 'A' && this_char <= 'Z') {
@@ -26,20 +24,18 @@ string solution(string new_id) {
 		// step 2
 		if ( (this_char < 'a' || this_char > 'z') && (this_char < '0' || this_char > '9') && this_char != '-' && this_char != '_' && this_char != '.') {
 			answer.erase(i, 1);
-			len_id--;
 			continue;
 		}
 	}
 
-	if (len_id == 0) {
+	if (answer.length() == 0) {
 		answer = "a";
-		len_id = 1;
 	}
 
 	// step 3
 	int cnt_dot = 0;
 	int pos_dot = -1;
-	for (int i = 0; i < len_id; i++) {
+	for (int i = 0; i < answer.length(); i++) {
 		if (answer.at(i) == '.') {
 			cnt_dot++;
 			if (pos_dot == -1) pos_dot = i;
@@ -47,7 +43,6 @@ string solution(string new_id) {
 		else {
 			if (cnt_dot > 1) {
 				answer.erase(pos_dot + 1, cnt_dot - 1);
-				len_id -= cnt_dot - 1;
 				i -= cnt_dot - 1;
 			}
 			cnt_dot = 0;
@@ -57,46 +52,35 @@ string solution(string new_id) {
 
 	if (cnt_dot > 1) {
 		answer.erase(pos_dot + 1, cnt_dot - 1);
-		len_id -= cnt_dot - 1;
-	}
-
-	if (len_id == 0) {
-		answer = "a";
-		len_id = 1;
 	}
 
 	// step 4
 	if (answer.at(0) == '.') {
 		answer.erase(0, 1);
-		len_id--;
 	}
 
 	// step 5
-	if (len_id == 0) {
+	if (answer.length() == 0) {
 		answer = "a";
-		len_id = 1;
 	}
 
 	// step 4
-	if (answer.at(len_id - 1) == '.') {
-		answer.erase(len_id - 1, 1);
-		len_id--;
+	if (answer.at(answer.length() - 1) == '.') {
+		answer.erase(answer.length() - 1, 1);
 	}
 
 	// step 6
-	if (len_id >= 16) {
-		answer.erase(15, len_id - 15);
-		len_id = 15;
-		if (answer.at(len_id - 1) == '.') {
-			answer.erase(len_id - 1, 1);
-			len_id--;
+	if (answer.length() >= 16) {
+		answer.erase(15, answer.length() - 15);
+		if (answer.at(answer.length() - 1) == '.') {
+			answer.erase(answer.length() - 1, 1);
 		}
 	}
 
 	// step 7
-	if (len_id <= 2) {
-		int redundance = 3 - len_id;
-		char ch = answer.at(len_id - 1);
+	if (answer.length() <= 2) {
+		int redundance = 3 - answer.length();
+		char ch = answer.at(answer.length() - 1);
 		string last(1, ch);
 		for (int i = 0; i < redundance; i++) {
 			answer.append(last);
